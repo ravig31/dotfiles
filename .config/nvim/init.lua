d7298b9a69716ac5224bd19374bf76b583ec48a7
ravig31 <ravindugamage7@gmail.com>
2025-05-21 02:34:07 +1000

    update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,7 +30,7 @@ vim.g.maplocalleader = "\\"
 require("lazy").setup({
   spec = {
     -- add your plugins here
-    require("plugins.kanagawa"),
+    require("plugins.vscodetheme"),
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
---
+++ .config/nvim/lua/plugins/vscodetheme.lua
@@ -0,0 +1,9 @@
+return {
+  {
+      "askfiy/visual_studio_code",
+      priority = 100,
+      config = function()
+          vim.cmd([[colorscheme visual_studio_code]])
+      end,
+  },
+}
