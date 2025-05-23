7b29332e61b7e120fc981745d583d757a188cf11
ravig31 <ravindugamage7@gmail.com>
2025-05-23 15:54:24 +1000

    update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -129,6 +129,16 @@ return {
                         },
                     })
                 end,
+
+                clangd = function()
+                    require('lspconfig').clangd.setup({
+                        on_attach = function(client, bufnr)
+                            client.server_capabilities.signatureHelpProvider = false
+                            on_attach(client, bufnr)
+                        end,
+                        capabilities = capabilities
+                    })
+                end
             },
         })
 
