dae04731a0f81d417dec8078dfab5f55b65a672f
ravig31 <ravindugamage7@gmail.com>
2025-05-27 08:59:08 +1000

    update

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -1,6 +1,19 @@
 vim.g.maplocalleader = "\\"
 vim.g.mapleader = " "
 
+-- Duplicate line(s)
+vim.keymap.set('n', '<A-S-j>', 'yyP', { noremap = true, silent = true, desc = "Duplicate current line down" })
+vim.keymap.set('v', '<A-S-j>', 'yP', { noremap = true, silent = true, desc = "Duplicate selected lines down" })
+
+-- Move line(s) up
+vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', { noremap = true, silent = true, desc = "Move current line up" })
+vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv", { noremap = true, silent = true, desc = "Move selected lines up" })
+
+-- Move line(s) down
+vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', { noremap = true, silent = true, desc = "Move current line down" })
+vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv", { noremap = true, silent = true, desc = "Move selected lines down" })
+
+
 -- select all ----
 vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true, silent = true, desc = 'Select All' })
 vim.keymap.set('v', '<C-a>', 'ggVG', { noremap = true, silent = true, desc = 'Select All' })
