65ea8b7154ac2ebc10baad6f555df428c00d1fc4
ravig31 <ravindugamage7@gmail.com>
2025-07-27 21:12:34 +1000

    rust changes

--- .config/nvim/lua/config/keybinds.lua
+++ .config/nvim/lua/config/keybinds.lua
@@ -44,3 +44,20 @@ vim.keymap.set('t', '<esc><esc>', '<C-\\><C-n>', { desc = "Shorcut for returning
 -- oil --
 vim.keymap.set("n", "<leader>-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
+-- nvim-dap --
+vim.keymap.set("n", "<Leader>dl", "<cmd>lua require'dap'.step_into()<CR>", { desc = "Debugger step into" })
+vim.keymap.set("n", "<Leader>dj", "<cmd>lua require'dap'.step_over()<CR>", { desc = "Debugger step over" })
+vim.keymap.set("n", "<Leader>dk", "<cmd>lua require'dap'.step_out()<CR>", { desc = "Debugger step out" })
+vim.keymap.set("n", "<Leader>dc", "<cmd>lua require'dap'.continue()<CR>", { desc = "Debugger continue" })
+vim.keymap.set("n", "<Leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "Debugger toggle breakpoint" })
+vim.keymap.set(
+	"n",
+	"<Leader>dd",
+	"<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+	{ desc = "Debugger set conditional breakpoint" }
+)
+vim.keymap.set("n", "<Leader>de", "<cmd>lua require'dap'.terminate()<CR>", { desc = "Debugger reset" })
+vim.keymap.set("n", "<Leader>dr", "<cmd>lua require'dap'.run_last()<CR>", { desc = "Debugger run last" })
+
+-- rustaceanvim
+vim.keymap.set("n", "<Leader>dt", "<cmd>lua vim.cmd('RustLsp testables')<CR>", { desc = "Debugger testables" })
--- .config/nvim/lua/plugins/one-liners.lua
+++ .config/nvim/lua/plugins/one-liners.lua
@@ -1,11 +1,11 @@
 return {
-		{ -- This helps with ssh tunneling and copying to clipboard
-				'ojroques/vim-oscyank',
-		},
-		{
-				'mluders/comfy-line-numbers.nvim'
-		},
-		{
-				'numToStr/Comment.nvim',
-		},
+	{ -- This helps with ssh tunneling and copying to clipboard
+		'ojroques/vim-oscyank',
+	},
+	{
+		'mluders/comfy-line-numbers.nvim'
+	},
+	{
+		'numToStr/Comment.nvim',
+	},
 }
