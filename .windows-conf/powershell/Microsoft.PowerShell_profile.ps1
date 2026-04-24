443d6ceda626ad7b0d9e156321fcfc78b4883e26
Ravindu Gamage <A30024524@agl.com.au>
2026-04-24 10:49:26 +1000

    update

--- .windows-conf/powershell/Microsoft.PowerShell_profile.ps1
+++ .windows-conf/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,11 +1,50 @@
 Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
 refreshenv
 
+
+function pyini() {
+	vim "~/AppData/Local/py.ini"
+}
+
+function shellagain() {
+	.$PROFILE
+}
+
 $env:Path += ";C:\ProgramData\chocolatey\bin"
 
-# kubectl aliases
+# kubectl 
 . "$(Split-Path -Path $PROFILE)\kubectl_aliases.ps1"
 function kalias() { cat "$(Split-Path -Path $PROFILE)\kubectl_aliases.ps1"}
+kubectl completion powershell | Out-String | Invoke-Expression
+
+
+function klon() {
+    param(
+        [string]$ns = "default",
+        [Parameter(ValueFromRemainingArguments = $true)]
+        $args
+    )
+
+    $pod = kgpon $ns --no-headers -o custom-columns=":metadata.name" | fzf
+
+    kubectl logs -n $ns $pod @args -f
+}
+
+function kcuc() {
+		kubectl config get-contexts --no-headers -o name |
+				fzf | 
+				ForEach-Object { kubectl config use-context $_ }
+}
+
+function kpfn() {
+	param([string]$ns = "default")
+	kubectl port-forward -n $ns $args
+}
+
+function kcgc() {
+	kubectl config get-contexts
+}
+
 # Basic aliases
 Set-Alias touch ni
 Set-Alias ll  Get-ChildItem
@@ -17,6 +56,9 @@ function zz { nvim $PROFILE }
 
 # git-aliases
 Import-Module git-aliases -DisableNameChecking
+function galias(){
+	Get-Git-Aliases | fzf
+}
 
 # poshgit 
 Import-Module posh-git
@@ -26,3 +68,19 @@ Set-PSReadlineKeyHandler -Key Tab -Function Complete
 
 # omp
 oh-my-posh init pwsh --config "~\dotfiles\.windows-conf\spaceship_custom.omp.json" | Invoke-Expression
+
+#poetry
+if (-not (Get-Command poetry -ErrorAction Ignore)) { $env:Path += ";C:\Users\A30024524\AppData\Roaming\Python\Scripts" }
+
+#uv
+if (!(Test-Path -Path $PROFILE)) {
+  New-Item -ItemType File -Path $PROFILE -Force
+}
+
+(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
+
+# remove aliases
+Remove-Item Alias:diff -Force -ErrorAction SilentlyContinue
+Remove-Item Alias:rm -Force -ErrorAction SilentlyContinue
+Remove-Item Alias:tree -Force -ErrorAction SilentlyContinue
+
