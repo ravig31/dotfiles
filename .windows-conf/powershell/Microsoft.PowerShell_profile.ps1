5a0f44b200412001b294dbc7d70c5510f343626b
Ravindu Gamage <ravindugamage7@gmail.com>
2026-04-26 11:00:37 +1000

    add setup script

--- .windows-conf/powershell/Microsoft.PowerShell_profile.ps1
+++ .windows-conf/powershell/Microsoft.PowerShell_profile.ps1
@@ -16,11 +16,11 @@ function shellagain() {
 
 $env:Path += ";C:\ProgramData\chocolatey\bin"
 
-# kubectl 
-. "$(Split-Path -Path $PROFILE)\kubectl_aliases.ps1"
-function kalias() { cat "$(Split-Path -Path $PROFILE)\kubectl_aliases.ps1"}
-kubectl completion powershell | Out-String | Invoke-Expression
-
+# # kubectl 
+# . "$(Split-Path -Path $PROFILE)\kubectl_aliases.ps1"
+# function kalias() { cat "$(Split-Path -Path $PROFILE)\kubectl_aliases.ps1"}
+# kubectl completion powershell | Out-String | Invoke-Expression
+#
 
 function klon() {
     param(
@@ -73,15 +73,15 @@ Set-PSReadlineKeyHandler -Key Tab -Function Complete
 # omp
 oh-my-posh init pwsh --config "~\dotfiles\.windows-conf\spaceship_custom.omp.json" | Invoke-Expression
 
-#poetry
-if (-not (Get-Command poetry -ErrorAction Ignore)) { $env:Path += ";C:\Users\A30024524\AppData\Roaming\Python\Scripts" }
-
-#uv
-if (!(Test-Path -Path $PROFILE)) {
-  New-Item -ItemType File -Path $PROFILE -Force
-}
-(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
-
+# #poetry
+# if (-not (Get-Command poetry -ErrorAction Ignore)) { $env:Path += ";C:\Users\A30024524\AppData\Roaming\Python\Scripts" }
+#
+# #uv
+# if (!(Test-Path -Path $PROFILE)) {
+#   New-Item -ItemType File -Path $PROFILE -Force
+# }
+# (& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
+#
 # remove aliases
 Remove-Item Alias:diff -Force -ErrorAction SilentlyContinue
 Remove-Item Alias:rm -Force -ErrorAction SilentlyContinue
